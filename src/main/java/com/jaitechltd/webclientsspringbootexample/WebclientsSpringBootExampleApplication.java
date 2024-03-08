218139653dd2e1d611426dc2b1f7ea151f8f3a9e
Raj Beemi <beemi.raja@gmail.com>
2024-03-08 23:11:52 +0000

    feat: added spring boot admin client details

--- src/main/java/com/jaitechltd/webclientsspringbootexample/WebclientsSpringBootExampleApplication.java
+++ src/main/java/com/jaitechltd/webclientsspringbootexample/WebclientsSpringBootExampleApplication.java
@@ -1,6 +1,5 @@
 package com.jaitechltd.webclientsspringbootexample;
 
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
