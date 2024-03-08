86e6a8048a5693de917df83ee3956554f85e2766
Raj Beemi <beemi.raja@gmail.com>
2024-03-08 22:21:46 +0000

    refactor: remove unused response dtos

--- src/main/java/com/jaitechltd/webclientsspringbootexample/webclient/PostcodeIoClient.java
+++ src/main/java/com/jaitechltd/webclientsspringbootexample/webclient/PostcodeIoClient.java
@@ -1,8 +1,6 @@
 package com.jaitechltd.webclientsspringbootexample.webclient;
 
-import com.jaitechltd.webclientsspringbootexample.dto.postcode.LatLongResponseDto;
 import com.jaitechltd.webclientsspringbootexample.dto.postcode.LocationResponseDto;
-import com.jaitechltd.webclientsspringbootexample.dto.postcode.ResponseDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
